92c22f3881adb74eaa6deffd1819e1c92bdfd11c
saniya-khan33015 <khansaniya33015@gmail.com>
2026-02-22 18:20:34 +0530

    Ci-Cd Fixes

--- main.tf
+++ main.tf
@@ -95,15 +95,6 @@ variable "aws_region" {
   type        = string
 }
 
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
 
 variable "aws_ami" {
   description = "AMI ID for EC2 instance"
